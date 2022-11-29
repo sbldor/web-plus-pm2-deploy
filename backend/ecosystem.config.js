26748b184cfb0001039b0e15a5e877fe687e7df2
sbldor <sydor.a@yandex.ru>
2022-11-30 01:47:23 +0300

    pm2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd ~/web-plus-pm2-deploy/source/backend/ && npm i && npm run build && pm2 restart ecosystem.config.js',
+      'post-deploy': 'cd ~/web-plus-pm2-deploy/backend/ && npm i && npm run build && pm2 restart ecosystem.config.js',
     },
   },
 };
