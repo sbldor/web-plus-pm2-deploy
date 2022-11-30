2a4b4aab1c09da9f2c6948e63aac2de617cee789
sbldor <sydor.a@yandex.ru>
2022-11-30 14:42:43 +0300

    pm2

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,6 +1,31 @@
+const dotenev = require('dotenv');
+
+dotenev.config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+  DEPLOY_REPO,
+} = process.env;
+
 module.exports = {
-  apps: [{
-    name: "app1",
-    script: './build/app.js',
-  }],
+  apps: [
+    {
+      name: "app-frontend",
+      script: './build/index.html',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': `cd ~/web-plus-pm2-deploy/frontend/ && npm i && NODE_OPTIONS='--openssl-legacy-provider' npm run build`,
+    }
+  }
 };
